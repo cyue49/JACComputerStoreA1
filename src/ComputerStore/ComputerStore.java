09e0de4070576d8e4a83e716a45a8febb4986cf1
Chen Yue <yuechen049@gmail.com>
2023-12-20 00:53:38 -0500

    added attributes to ComputerStore class

--- src/ComputerStore/ComputerStore.java
+++ src/ComputerStore/ComputerStore.java
@@ -1,4 +1,10 @@
 package ComputerStore;
 
+import java.util.ArrayList;
+
 public class ComputerStore {
+    private static final String PASSWORD = "password"; // password for the computer store
+    private int maxComputers; // maximum number of computers the Computer Store can hold
+    private int emptySlots; // remaining empty slots in the Computer Store
+    private ArrayList<Computer> inventory; // array list representing the inventory of the Computer Store
 }
